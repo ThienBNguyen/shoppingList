d28b4ffb325996dfde50121157c177a32430e096
Thien Nguyen <thien.nguyen30191@gmail.com>
2022-03-21 16:43:59 -0500

    fully intergrate with the backend with redux

--- client/src/components/ShoppingList.jsx
+++ client/src/components/ShoppingList.jsx
@@ -17,11 +17,11 @@ export class ShoppingList extends Component {
             <Container>
                 <ListGroup>
                     <TransitionGroup className="shopping-list">
-                        {items.map(({ id, name }) =>
-                            <CSSTransition key={id} timeout={500} classNames="fade">
+                        {items.map(({ _id, name }) =>
+                            <CSSTransition key={_id} timeout={500} classNames="fade">
                                 <ListGroupItem>
                                     <Button className="remove-btn" color="danger" onClick={
-                                        this.onDeleteClick.bind(this, id)
+                                        this.onDeleteClick.bind(this, _id)
                                     }> &times;</Button>
                                     {name}
                                 </ListGroupItem>
--- client/src/flux/reducer/itemReducer.js
+++ client/src/flux/reducer/itemReducer.js
@@ -1,73 +1,34 @@
-// import {
-//     GET_ITEMS,
-//     ADD_ITEM,
-//     DELETE_ITEM,
-//     ITEMS_LOADING
-// } from '../actions/types';
-// import { IAction, IItem } from '../../types/interfaces';
-
-// const initialState = {
-//     items: [],
-//     loading: false
-// };
-
-// interface IState {
-//     items: IItem[];
-// }
-
-// export default function (state: IState = initialState, action: IAction) {
-//     switch (action.type) {
-//         case GET_ITEMS:
-//             return {
-//                 ...state,
-//                 items: action.payload,
-//                 loading: false
-//             };
-//         case DELETE_ITEM:
-//             return {
-//                 ...state,
-//                 items: state.items.filter(item => item._id !== action.payload)
-//             };
-//         case ADD_ITEM:
-//             return {
-//                 ...state,
-//                 items: [action.payload, ...state.items]
-//             };
-//         case ITEMS_LOADING:
-//             return {
-//                 ...state,
-//                 loading: true
-//             };
-//         default:
-//             return state;
-//     }
-// }
-import { v4 as uuid } from 'uuid';
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../../actions/types';
+// import { v4 as uuid } from 'uuid';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from '../../actions/types';
 const initialState = {
-	items: [
-		{ id: uuid(), name: 'eggs' },
-		{ id: uuid(), name: 'milk' },
-		{ id: uuid(), name: 'tools' },
-		{ id: uuid(), name: 'bread' }
-	]
+	items: [],
+	loading: false
 };
 
 const itemReducer = (state = initialState, action) => {
+	console.log(action);
+
 	switch (action.type) {
 		case GET_ITEMS:
 			return {
-				...state
+				...state,
+				items: action.payload,
+				loading: false
 			};
 		case DELETE_ITEM:
 			return {
 				...state,
-				items: state.items.filter((item) => item.id !== action.payload)
+				items: state.items.filter((item) => item._id !== action.payload)
 			};
 		case ADD_ITEM:
 			return {
 				...state,
 				items: [ action.payload, ...state.items ]
+			};
+		case ITEMS_LOADING:
+			return {
+				...state,
+				loading: true
 			};
 
 		default:
