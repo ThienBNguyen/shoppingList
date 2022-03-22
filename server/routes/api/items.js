b8ce811a1d9efb10319401c715a24ff77ef52095
Thien Nguyen <thien.nguyen30191@gmail.com>
2022-03-22 15:18:35 -0500

    update backend include user route and user auth

--- server/routes/api/items.js
+++ server/routes/api/items.js
@@ -1,30 +1,29 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
-
+const auth = require('../../middleware/auth');
 // Item Model
-const Item = require('../../models/item')
+const Item = require('../../models/Item');
 
 // @route get api/items
 // @desc Get All Items
 // @access Pulbic
 router.get('/', (req, res) => {
-    Item.find().sort({ date: -1 }).then(items => res.json(items));
+	Item.find().sort({ date: -1 }).then((items) => res.json(items));
 });
 
-router.post('/', (req, res) => {
-    // get the object from the bodyparser
-    const newItem = new Item({
-        name: req.body.name
-    })
-    newItem.save().then(item => res.json(item))
-    //res.json() give 200 response
-})
-router.delete('/:id', (req, res) => {
-    // get the object from the bodyparser
-    Item.findById(req.params.id)
-        .then(item => item.remove()
-            .then(() => res.json({ success: true })))
-        .catch(err => res.status(404).json({ success: false }))
-})
+router.post('/', auth, (req, res) => {
+	// get the object from the bodyparser
+	const newItem = new Item({
+		name: req.body.name
+	});
+	newItem.save().then((item) => res.json(item));
+	//res.json() give 200 response
+});
+router.delete('/:id', auth, (req, res) => {
+	// get the object from the bodyparser
+	Item.findById(req.params.id)
+		.then((item) => item.remove().then(() => res.json({ success: true })))
+		.catch((err) => res.status(404).json({ success: false }));
+});
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const items = require('./routes/api/items');
+const users = require('./routes/api/users');
+const auth = require('./routes/api/auth');
+const config = require('config');
 const path = require('path');
 require('dotenv').config;
 const app = express();
 //bodyparser middleware
-app.use(bodyParser.json());
-
+app.use(express.json());
+const db = config.get('mongoURI');
 const uri =
 	'mongodb+srv://mongoconnect:Tn123456789@yelpcamp.7z2ix.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 mongoose
-	.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+	.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() => console.log('mongoDB Connected...')).catch;
 
 //Use Routes
 app.use('/api/items', items);
+app.use('/api/users', users);
+app.use('/api/auth', auth);
+
 //server static assets if in production
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
